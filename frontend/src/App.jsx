747b664ed924eb0efe788bc4b4b70548d3bc9c8b
Chayan Chandro Sutrodhar <chayansd5656@gmail.com>
2024-04-27 23:28:05 +0600

    Added frontend repo

---
+++ frontend/src/App.jsx
@@ -0,0 +1,16 @@
+import {BrowserRouter} from "react-router-dom";
+
+export default function App() {
+  return (
+    <>
+       <BrowserRouter>
+        <Routes>
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/signin" element={<Signin />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/send" element={<SendMoney />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  )
+}
