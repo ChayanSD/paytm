690737ad5b40dc66cc01b190f69724b7592266cb
Chayan Chandro Sutrodhar <chayansd5656@gmail.com>
2024-04-28 20:16:00 +0600

    Complete the frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,8 @@
-import {BrowserRouter} from "react-router-dom";
+import {BrowserRouter , Route , Routes} from "react-router-dom";
+import { Signup } from "./pages/Signup";
+import { Signin } from "./pages/Signin";
+import { SendMoney } from "./pages/SendMoney";
+import { Dashboard } from "./pages/Dashboard";
 
 export default function App() {
   return (
@@ -6,7 +10,7 @@ export default function App() {
        <BrowserRouter>
         <Routes>
           <Route path="/signup" element={<Signup />} />
-          <Route path="/signin" element={<Signin />} />
+          <Route path="/signin" element={<Signin/>} />
           <Route path="/dashboard" element={<Dashboard />} />
           <Route path="/send" element={<SendMoney />} />
         </Routes>
